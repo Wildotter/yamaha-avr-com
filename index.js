98a8641f4a970fb317313bcb46fd20f777d10831
stephan <sithrulez>
2022-07-10 12:57:21 +0200

    Define Exports of Module

---
+++ index.js
@@ -0,0 +1,5 @@
+require('./src/interactors/av-receiver-inter-actor');
+const {AvReceiverInterActor} = require("./src/interactors/av-receiver-inter-actor");
+const {TunerInterActor} = require("./src/interactors/tuner-inter-actor");
+exports.avReceiverInterActor = AvReceiverInterActor;
+exports.tunerInterActor = TunerInterActor;
